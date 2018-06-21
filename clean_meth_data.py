738135741943d506b21d2c1c796991e579e570da
dengxiaodi <dixiaodeng@gmail.com>
2018-06-21 14:11:43 +0800

    filename bugs

--- clean_meth_data.py
+++ clean_meth_data.py
@@ -9,7 +9,7 @@ import numpy as np
 from numpy import genfromtxt
 
 def clean_meth_data(filename_input, filename_output) :
-	methy_data = genfromtxt(filename, delimiter = '\t', skip_header = 1,
+	methy_data = genfromtxt(filename_input, delimiter = '\t', skip_header = 1,
 		usecols = (0, 1, 2, 3),
 		dtype = ('S12', '<f8', 'S8', '<u8'), 
 		names = ('ref', 'beta', 'chr', 'pos'))
@@ -22,7 +22,7 @@ def clean_meth_data(filename_input, filename_output) :
 
 	methy_data = methy_data[~np.isnan(methy_data['beta'])]
 	if filename_output == None :
-		filename_output = os.path.splitext(os.path.basename(filename_input))[0] + '.csv'
+		filename_output = os.path.splitext(os.path.basename(filename_input))[0] + '.clean.csv'
 	np.savetxt(filename_output, 
 		methy_data, 
 		fmt=('%s, %f, %s, %ld'), 
