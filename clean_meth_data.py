407afabd5c995876a31a668d8abab4b0fb78f585
dengxiaodi <dixiaodeng@gmail.com>
2018-06-21 16:49:27 +0800

    trim clean data adjust

--- clean_meth_data.py
+++ clean_meth_data.py
@@ -11,7 +11,7 @@ from numpy import genfromtxt
 def clean_meth_data(filename_input, filename_output) :
 	methy_data = genfromtxt(filename_input, delimiter = '\t', skip_header = 1,
 		usecols = (0, 1, 2, 3),
-		dtype = ('S', '<f8', 'S', '<u8'), 
+		dtype = ('|S12', '<f8', '|S8', '<u8'), 
 		names = ('ref', 'beta', 'chr', 'pos'))
 
 	# filter out all unknown chrs
