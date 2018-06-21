1ff23d4566906bb629e3d6b7d9352799c711f9a0
dengxiaodi <dixiaodeng@gmail.com>
2018-06-21 10:42:04 +0800

    rebuild cg index

--- build_cg_index.py
+++ build_cg_index.py
@@ -1,42 +1,68 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-def build_cg_index(filename_input, filename_output) :
-	# load sequence file
+import argparse
+import os
+import numpy as np
+import re
 
-	chrname = ''
-	seq = ''
-	with open(filename_input, 'r') as seq_file :
-		
-		for line in seq_file:
+def build_cg_index(filename_ref_seq, filename_cg_index) :
+	# load sequence file
+	print('[*] loading reference sequence')
+	dict_ref_seq = {}
+	with open(filename_ref_seq, 'r') as ref_seq_file :
+		chrname = ''
+		seq = ''
+		for line in ref_seq_file:
 			if(line[0] == '>'):
+				
+				# save current seq for current chr
+
+				if(chrname != ''):
+					dict_ref_seq[chrname] = seq
+				
+				# new chrname & seq
+
 				chrname = line[1:].strip()
+				seq = ''
+				print('    loading reference sequence: {0}'.format(chrname))
 			else:
 				seq += line.strip().upper()
+		
+		# write the last chr
+
+		if(chrname != ''):
+			dict_ref_seq[chrname] = seq
+	ref_seq_file.close()
 
 	# build cg indexes
-	
-	indexes = [m.start() for m in re.finditer("CG", seq)]
-	indexes = np.array(indexes)
-	if filename_output == None :
-		filename_output = chrname + '.cgidx'
-	np.savez(filename_output, chrsize = len(seq), indexes = indexes)
 
-	return filename_output
+	print('[*] building cg indexes')
+	dict_cg_indexes = {}
+	offset = 0
+	for chrname in dict_ref_seq:
+		print('    building cg indexes for reference sequence: {0}'.format(chrname))
+		seq = dict_ref_seq[chrname]
+		chrlen = len(seq)
+		offset += chrlen
+		indexes = [m.start() for m in re.finditer("CG", seq)]
+		dict_cg_indexes[chrname] = (offset, chrlen, np.array(indexes))
+	
+	if filename_cg_index == None :
+		filename_cg_index = os.path.splitext(os.path.basename(filename_ref_seq))[0] + '.cgidx'
+	np.savez(filename_cg_index, dict_cg_indexes)
+
+	return filename_cg_index
 
 if __name__ == "__main__":
-	import argparse
-	import os
-	import numpy as np
-	import re
 
 	# command line arguments
 
 	parser = argparse.ArgumentParser(description = "Build CpG indexes from reference genomes")
 	parser.add_argument("-i", "--input", dest = "input", required = True,
-		help = "reference genome file by chromsome chr.fa", metavar = "FILE")
+		help = "reference genome file", metavar = "FILE")
 	parser.add_argument("-o", "--output", dest = "output", required = False,
-		help = "output cg index file name chr.cgidx", metavar = "FILE")
+		help = "output cg index file name .cgidx", metavar = "FILE")
 
 	args = parser.parse_args()
 	filename_input = args.input
@@ -45,11 +71,11 @@ if __name__ == "__main__":
 	# parameter check
 
 	if not os.path.isfile(filename_input) :
-		print("reference genome .fa file \"" + filename_input + "\" does not exist!")
+		print("[~] reference genome .fa file \"" + filename_input + "\" does not exist!")
 		exit(-1)
 
 	if filename_output != None and os.path.isfile(filename_output) :
 		os.unlink(filename_output)
 
 	filename_output = build_cg_index(filename_input, filename_output)
-	print('cg index file "' + filename_output + '" write complete.')
+	print('[*] cg index file "' + filename_output + '" write complete.')
